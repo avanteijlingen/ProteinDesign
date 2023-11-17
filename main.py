373401145c15a54e62cdebb28919ed61306f667f
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2023-11-17 03:02:15 +0000

    Update main.py
    cleaning and documentation

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ Created on Fri Nov 10 20:05:38 2023
 @author: Alex
 """
 import MDAnalysis as mda
-import urllib, os, subprocess, sys, shutil, copy, re, time, json, pandas, requests
+from MDAnalysis.analysis.hydrogenbonds.hbond_analysis import HydrogenBondAnalysis as HBA
+import urllib, os, subprocess, sys, shutil, copy, re, json, pandas
 from ase import Atoms
 import numpy as np
-from sklearn.metrics import euclidean_distances, r2_score
-import matplotlib.pyplot as plt
+from sklearn.metrics import euclidean_distances
 from pathlib import Path
 import torch
 from AIMNet2.calculators.aimnet2ase import AIMNet2Calculator
-from MDAnalysis.analysis.hydrogenbonds.hbond_analysis import HydrogenBondAnalysis as HBA
-
-import warnings
-warnings.filterwarnings("ignore")
-
 from dotenv import load_dotenv
+import warnings
+#warnings.filterwarnings("ignore") # MDAnalysis throws many warnings
 
 # Environmental variables check
 load_dotenv()
@@ -29,15 +26,10 @@ for var in ["NAMD", "VMDTOP", "PSFGEN"]:
 import contactarea
 import peptideutils as pu 
 
-# =============================================================================
-# https://www.ks.uiuc.edu/Training/Tutorials/namd/FEP/tutorial-FEP.pdf
-# page 20 Mutation of tyrosine into alanine
-# =============================================================================
 
 vmd_dir = os.environ["VMDTOP"]
 psfgen = os.environ["PSFGEN"]
 namd = os.environ["NAMD"]
-
 
 Angstrom2Bohr = 1.88973
 eV2kcalmol    = 23.0609
@@ -54,7 +46,19 @@ Chains = {"6M0J": {"A": "ACE2",
 
 
 
-def writepgn(fname, pdbs, segids, outfile):
+def writepgn(fname: str, pdbs: list, segids: list, outfile: str) -> None:
+    """
+    Parameters
+    ----------
+    fname : str
+        the file name (path) of the pgn file you want to write.
+    pdbs : list
+        list of pdb files to be included in the pgn.
+    segids : list
+        the segids to be assigned to the pdb files included in the pgn.
+    outfile : str
+        the name that psfgen will write the output of {fname}.pgn to.
+    """
     pgn = open(fname,'w')
     pgn.write("package require psfgen")
     pgn.write("\n")
@@ -74,14 +78,34 @@ def writepgn(fname, pdbs, segids, outfile):
     pgn.close()
     
 def readin(fpath: str) -> str:
-	f = open(fpath)
-	content = f.read()
-	f.close()
-	return content
+    """    
+    Parameters
+    ----------
+    fpath : str
+        file path of the text file to read.
+
+    Returns
+    -------
+    str
+        the contents of the file.
+    """
+    f = open(fpath)
+    content = f.read()
+    f.close()
+    return content
 
 def from_template(fname: str, original: list, new: list) -> None:
     """
     Replace every instance of original[n] with new[n]
+
+    Parameters
+    ----------
+    fname : str
+        file name of the file to modify.
+    original : list
+        the texts to be replaced.
+    new : list
+        the corrisponding replacement texts.
     """
     assert len(original) == len(new)
     content = readin(fname)
@@ -91,11 +115,14 @@ def from_template(fname: str, original: list, new: list) -> None:
         outfile.write(content)
 
 class measure_interface:
-    def download_pdb(self):
+    def download_pdb(self): # Download the RCSB id pdb from RCSB
         if not os.path.exists(f"{self.active_folder}/{self.idx}.pdb"):
             urllib.request.urlretrieve(f"http://files.rcsb.org/download/{self.idx}.pdb", f"{self.active_folder}/{self.idx}.pdb") 
     
     def psf(self):
+        """
+        Generate a forcefield topology from the pdb
+        """
         if os.path.exists(f"{self.active_folder}/{self.idx}_psfgen.psf"):
             self.fix_psf(f"{self.active_folder}/{self.idx}_psfgen.psf") # temp, once off fix
             return None
@@ -115,6 +142,9 @@ class measure_interface:
         self.fix_psf(f"{self.active_folder}/{self.idx}_psfgen.psf")
     
     def fix_psf(self, psf_file):
+        """
+        repair resids in psf's that contain letters'
+        """
         # psfgen is adding numbers to some resids in the psf for 7Z0X chainID L and H, so we will just convert these (82A -> 82)
         # For this program this will not be a problem, at most it could increase compute time
         with open(psf_file) as input_psf:
@@ -132,18 +162,23 @@ class measure_interface:
                 output_psf.write("\n")
 
     def Minimize(self, min_input = "Minimize.namd"):
+        """
+        Run a simulation of the system in NAMD
+        Parameters
+        ----------
+        min_input : TYPE, optional
+            The name of the template NAMD input file. The default is "Minimize.namd".
+        """
         # Run a short minimization of the system
         if not os.path.exists(f"{self.active_folder}/Minimization.coor"):
             shutil.copy(min_input, f"{self.active_folder}/Minimize.namd")
             from_template(f"{self.active_folder}/Minimize.namd", ["INPUT", "PARAM_DIR"], [f"{self.idx}_psfgen", Path("parameters").absolute().as_posix()])
-            #subprocess.check_output([namd, "+p4", f"{self.active_folder}/Minimize.namd", ">", f"{self.active_folder}/Minimize.log"])
             os.system(" ".join([namd, "+p10", f"{self.active_folder}/Minimize.namd", ">", f"{self.active_folder}/Minimize.log"]))
-        # =============================================================================
-        #     ps = subprocess.Popen([namd, "+p4", f"{self.active_folder}/Minimize.namd", ">", f"{self.active_folder}/Minimize.log"], stdout=subprocess.PIPE)
-        #     ps.wait()
-        # =============================================================================
 
     def MeasureHBonds(self):
+        """
+        Measure the number of hydrogen bonds between the Spike protein and the protein it is complexed to
+        """
         self.hbonds = np.ndarray((0, 4), dtype=np.int64)
         self.hbonds_detailed = pandas.DataFrame()
         hbonds_calc = HBA(universe=self.U)
@@ -168,7 +203,6 @@ class measure_interface:
                     self.hbonds_detailed.at[i, "donor"] = "not Spike"
                     self.hbonds_detailed.at[i, "donor resname"]    = self.Receptor_interface.select_atoms(f"id {row[0]}").resnames
                     self.hbonds_detailed.at[i, "acceptor resname"] = self.Spike_interface.select_atoms(f"id {row[2]}").resnames
-                    
                 i+=1
             self.hbonds = np.vstack((self.hbonds, counts))
         self.hbonds_detailed["donor id"] = self.hbonds_detailed["donor id"].astype(np.int64)
@@ -176,11 +210,11 @@ class measure_interface:
         self.hbonds_detailed["acceptor id"] = self.hbonds_detailed["acceptor id"].astype(np.int64)
         self.hbonds_calc = hbonds_calc
         self.score["hbonds"] = self.hbonds.shape[0]
-        
-    def MeasureHydrophobicInteractions(self):
-        pass
     
-    def MeasureBindingEnergy(self):               
+    def MeasureBindingEnergy(self):       
+        """
+        Measure the interface binding energy using AIMNet2
+        """        
         self.Spike_ase.calc = self.AIMNet2_calc
         self.Receptor_ase.calc = self.AIMNet2_calc
         Complex = self.Receptor_ase+self.Spike_ase
@@ -200,12 +234,28 @@ class measure_interface:
         BindingEnergy = (Complex_E - (Receptor_E + Spike_E)) * eV2kcalmol
         self.score["BindingEnergy"] = BindingEnergy
         
-    def MeasureInterface(self):       
+    def MeasureInterface(self):
+        """
+        Run and stash the 3 measurement tools:
+            Hydrogen bond count
+            AIMNet2 binding energy
+            Protein-protein contact surface area
+        """    
         self.MeasureHBonds()
         self.MeasureBindingEnergy()
         self.score["contact surface area"] = self.surface_contact.calculate(self.Spike_ase, self.Receptor_ase)
     
     def FindInitialInterface(self):
+        """
+        Determine the Spike protein resids that are involved in the protein-protein interface, 
+        only used in the initial run.
+        Afterwards it is stored in Data.json
+
+        Returns
+        -------
+        np.ndarray
+            Spike protein resids involved in the interface.
+        """
         # First determine which residues are part of the interface
         d = euclidean_distances(self.Spike.positions, self.Receptor.positions)
         Spike_interface = self.Spike[d.min(axis=1) < self.interface_cutoff]
@@ -213,12 +263,18 @@ class measure_interface:
         return np.unique(Spike_interface.resids)
         
     def FindInterface(self):
+        """
+        Find the corresponding Receptor interface resids based on their distance to the Spike protein interface residues
+        """
         # Use this if we have already determined which resids are part of the interface
         d = euclidean_distances(self.Spike.select_atoms(f"resid {self.spike_interface_resids}").positions, self.Receptor.positions)
         Receptor_interface_partial = self.Receptor[d.min(axis=0) < self.interface_cutoff]
         self.Receptor_resids = " ".join(np.unique(Receptor_interface_partial.resids).astype(np.str_))
 
     def BuildInterface(self):
+        """
+        Build the MDAnalsis and ASE models of the spike and receptor interfaces
+        """
         self.Spike_interface = self.Spike.select_atoms(f"resid {self.spike_interface_resids}")
         self.Receptor_interface = self.Receptor.select_atoms(f"resid {self.Receptor_resids}")
         mda.Merge(self.Spike_interface, self.Receptor_interface).select_atoms("all").write(f"{self.active_folder}/Interface.pdb")
@@ -230,13 +286,25 @@ class measure_interface:
         if self.original_seq is None:
             self.original_seq = copy.copy(self.interface_seq)
             
-    def load_universe(self, coord_file = "Minimization.coor"):
+    def load_universe(self, coord_file: str = "Minimization.coor"):
+        """
+        Load an mdanalysis universe from the current state and coord_file
+        
+        Parameters
+        ----------
+        coord_file : str, optional
+            The local file name of the system coordinates to load. The default is "Minimization.coor".
+
+        """
         self.U = mda.Universe(f"{self.active_folder}/{self.idx}_psfgen.psf", f"{self.active_folder}/{coord_file}")
         self.U.trajectory[-1]
         self.Spike = self.U.select_atoms(f"segid {self.spike_chainID}")
         self.Receptor = self.U.select_atoms(f"segid {self.receptor_chainID}")
     
     def Mutate(self):
+        """
+        Randomly mutate the spike interface residues (only 1)
+        """
         sel = np.random.choice(np.arange(len(self.interface_seq)))
         self.interface_seq = list(self.interface_seq)
         new_res = self.interface_seq[sel]
@@ -246,6 +314,10 @@ class measure_interface:
         self.interface_seq = "".join(self.interface_seq)
         
     def MakeMutation(self):
+        """
+        Apply the current self.interface_seq
+        Build the folder, write the pdbs and pgn and make the psf file
+        """
         self.active_folder = f"MD/{self.idx}_{self.interface_seq}"
         os.makedirs(self.active_folder, exist_ok=True)
         self.Spike_interface.residues.resnames = pu.translate1to3(self.interface_seq).split("-") # This propogates all the way up the universe
@@ -260,11 +332,14 @@ class measure_interface:
                  pdbs, 
                  segids, 
                  f"{self.active_folder}/{self.idx}_psfgen")
-        x = subprocess.check_output([psfgen, f"{self.active_folder}/{self.idx}.pgn"])
+        subprocess.check_output([psfgen, f"{self.active_folder}/{self.idx}.pgn"])
         assert os.path.exists(f"{self.active_folder}/{self.idx}_psfgen.psf"), f"Couldnt make: {self.active_folder}/{self.idx}_psfgen.psf"
         self.fix_psf(f"{self.active_folder}/{self.idx}_psfgen.psf")
         
     def reset_seq(self):
+        """
+        reset the interface sequence to the wild (original) sequence
+        """
         self.interface_seq = copy.copy(self.original_seq)
         
     def __init__(self, idx, device):
@@ -286,10 +361,7 @@ class measure_interface:
                 self.receptor_chainID = self.receptor_chainID + chainID + " "
         self.spike_chainID = self.spike_chainID.strip() # Removing trailing ' '
         self.receptor_chainID = self.receptor_chainID.strip()
-# =============================================================================
-#         print("receptor_chainID:", self.receptor_chainID)
-#         print("spike_chainID:", self.spike_chainID)
-# =============================================================================
+
         self.interface_cutoff = 10.0 # Angstrom, We need to trim these proteins down to just those at the interface to fit the dispersion calculation in memory
         
         # Load AIMNet2 model for energy calculations
@@ -303,27 +375,18 @@ class measure_interface:
         self.score = {}
 
 
-# =============================================================================
-# def load_data_from_api(host="http://localhost:5000"):
-#     api_url = f"{host}/ProteinDesign"    
-#     response = requests.get(api_url)
-#     return response.json()
-# 
-# def post_entry_to_api(idx, seq, entry_data, host="http://localhost:5000"):
-#     api_url = f"{host}/ProteinDesign/{idx}/{seq}"
-#     print(api_url)
-#     post_data = {"Source": "API_test",
-#                 "hbonds": 6,
-#                 "BindingEnergy": -32.35931305264907,
-#                 "contact surface area": 31.93952531149623
-#                 }
-#     response = requests.post(api_url, json=entry_data)
-#     #print(response)
-#     #print(response.json())
-#     print(response.status_idx)
-# =============================================================================
+
 ## Have to patch this due to lack of networking
-def load_data_from_api():
+def load_data_from_api() -> dict:
+    """
+    
+
+    Returns
+    -------
+    dict
+        The data of all systems measured.
+
+    """
     if os.path.exists("Data.json"):
         with open("Data.json") as jin:
             Data = json.load(jin)
@@ -332,6 +395,18 @@ def load_data_from_api():
     return Data
 
 def post_entry_to_api(Data: dict, idx: str, seq: str, entry_data: dict):
+    """
+    Parameters
+    ----------
+    Data : dict
+        The data dictionary of all systems.
+    idx : str
+        the RCSB id to assign new data to.
+    seq : str
+        the residue sequence to store new data to.
+    entry_data : dict
+        dictionary of the scores to add.
+    """
     Data[idx][seq] = entry_data
     shutil.copy("Data.json", "Data.json.bak")
     with open("Data.json", 'w') as jout: jout.write(json.dumps(Data, indent=4))
@@ -384,28 +459,28 @@ if __name__ == "__main__":
             Complex.MeasureInterface()
             
             Data = load_data_from_api() # always reload before posting incase there is new data from another source
-            entry = {"Source": "ML"}
+            entry = {"Source": "XRD"}
             entry.update(Complex.score)
             post_entry_to_api(Data, idx, Complex.interface_seq, entry)
             Data = load_data_from_api() # always reload after posting incase there is new data from another source
-            
+        
+        
         Complex.Minimize()
         Complex.load_universe()
-        
         Complex.FindInterface() # If we have already set self.spike_interface_resids then use this
         Complex.BuildInterface()
         #Make measurements and store them
         if Complex.interface_seq not in Data[idx]:
             Complex.MeasureInterface()
             Data = load_data_from_api() # always reload before posting incase there is new data from another source
-            entry = {"Source": "ML"}
+            entry = {"Source": "Inital"}
             entry.update(Complex.score)
             post_entry_to_api(Data, idx, Complex.interface_seq, entry)
             Data = load_data_from_api() # always reload after posting incase there is new data from another source
         else:
             print("Already have data for:", idx, Complex.interface_seq)
             
-    sys.exit()
+
     # Randomly mutate residues and record the interface
     while len(Data["7Z0X"]) < 1000:
         print(f"Random iteration: ", len(Data["7Z0X"])-2)
